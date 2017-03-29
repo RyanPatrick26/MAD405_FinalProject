54f65161de4fc918e115853eaf793b2f7fa6d40a
Nicholas Allaire <nicholas.allaire85@stclairconnect.ca>
2017-03-29 17:55:08 -0400

    #11: Added Getters & Setters for Character class

--- app/src/main/java/confusedgriffinproductions/dudesinadungeon/Character.java
+++ app/src/main/java/confusedgriffinproductions/dudesinadungeon/Character.java
@@ -29,8 +29,163 @@ public class Character {
     private ArrayList<Item> items;
     private ArrayList<Spell> spells;
 
-    
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public String getCharClass() {
+        return charClass;
+    }
+
+    public void setCharClass(String charClass) {
+        this.charClass = charClass;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getAgility() {
+        return agility;
+    }
+
+    public void setAgility(int agility) {
+        this.agility = agility;
+    }
+
+    public int getResilience() {
+        return resilience;
+    }
+
+    public void setResilience(int resilience) {
+        this.resilience = resilience;
+    }
+
+    public int getLuck() {
+        return luck;
+    }
+
+    public void setLuck(int luck) {
+        this.luck = luck;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public int getFighting() {
+        return fighting;
+    }
+
+    public void setFighting(int fighting) {
+        this.fighting = fighting;
+    }
+
+    public int getGambling() {
+        return gambling;
+    }
+
+    public void setGambling(int gambling) {
+        this.gambling = gambling;
+    }
+
+    public int getShooting() {
+        return shooting;
+    }
+
+    public void setShooting(int shooting) {
+        this.shooting = shooting;
+    }
+
+    public int getLying() {
+        return lying;
+    }
+
+    public void setLying(int lying) {
+        this.lying = lying;
+    }
+
+    public int getCasting() {
+        return casting;
+    }
+
+    public void setCasting(int casting) {
+        this.casting = casting;
+    }
+
+    public int getAcrobatics() {
+        return acrobatics;
+    }
+
+    public void setAcrobatics(int acrobatics) {
+        this.acrobatics = acrobatics;
+    }
+
+    public int getSneaking() {
+        return sneaking;
+    }
+
+    public void setSneaking(int sneaking) {
+        this.sneaking = sneaking;
+    }
+
+    public int getCrafting() {
+        return crafting;
+    }
+
+    public void setCrafting(int crafting) {
+        this.crafting = crafting;
+    }
+
+    public int getSurvival() {
+        return survival;
+    }
+
+    public void setSurvival(int survival) {
+        this.survival = survival;
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
+    }
+
+    public ArrayList<Spell> getSpells() {
+        return spells;
+    }
+
+    public void setSpells(ArrayList<Spell> spells) {
+        this.spells = spells;
+    }
 }
