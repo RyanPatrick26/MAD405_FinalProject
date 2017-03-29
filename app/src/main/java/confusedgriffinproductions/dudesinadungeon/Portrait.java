b608c139fa8b809a35399cacf5bf0e37889d736e
Nicholas Allaire <nicholas.allaire85@stclairconnect.ca>
2017-03-29 18:42:38 -0400

    #11: Renamed PlayerPicture to Portrait for readability

--- app/src/main/java/confusedgriffinproductions/dudesinadungeon/Portrait.java
+++ app/src/main/java/confusedgriffinproductions/dudesinadungeon/Portrait.java
@@ -1,31 +1,31 @@
 package confusedgriffinproductions.dudesinadungeon;
 
 /**
- * PlayerPicture Class
+ * Portrait Class
  * Users will be able to take a picture of themselves or anything to represent
  * themselves in the game
  *
  * @author Nicholas Allaire
  * @version 1.0
  */
-public class PlayerPicture {
+public class Portrait {
 
-    // Declare PlayerPicture properties
+    // Declare Portrait properties
     private int id;
     private String resource;
 
     // Constructors
     // Empty constructor
-    public PlayerPicture() {}
+    public Portrait() {}
 
     // Constructor w/everything
-    public PlayerPicture(int id, String resource){
+    public Portrait(int id, String resource){
         this.id = id;
         this.resource = resource;
     }
 
     // Constructor w.o/ id
-    public PlayerPicture(String resource){
+    public Portrait(String resource){
         this.resource = resource;
     }
 
