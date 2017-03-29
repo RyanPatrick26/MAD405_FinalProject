7980db98be2c85990a839aa15725775409c60c5f
Nicholas Allaire <nicholas.allaire85@stclairconnect.ca>
2017-03-29 17:53:43 -0400

    #11: Added all properties for Character class

--- app/src/main/java/confusedgriffinproductions/dudesinadungeon/Character.java
+++ app/src/main/java/confusedgriffinproductions/dudesinadungeon/Character.java
@@ -1,10 +1,36 @@
 package confusedgriffinproductions.dudesinadungeon;
 
+import java.util.ArrayList;
+
 public class Character {
 
     // Declare properties for Characters
     private int id;
+    private String name;
+    private String race;
+    private String charClass;
+    // Stats
+    private int strength;
+    private int agility;
+    private int resilience;
+    private int luck;
+    private int intelligence;
+    // Skills
+    private int fighting;
+    private int gambling;
+    private int shooting;
+    private int lying;
+    private int casting;
+    private int acrobatics;
+    private int sneaking;
+    private int crafting;
+    private int survival;
+    // Items and Spells
+    private ArrayList<Item> items;
+    private ArrayList<Spell> spells;
+
     
+
 
 
 }
