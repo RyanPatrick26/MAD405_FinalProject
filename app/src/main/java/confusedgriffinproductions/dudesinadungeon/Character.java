f65840f9b62cdb74707ebcefc8a9acb659f66fcb
Nicholas Allaire <nicholas.allaire85@stclairconnect.ca>
2017-03-29 18:15:46 -0400

    #11: Created properties and constructors for Item

--- app/src/main/java/confusedgriffinproductions/dudesinadungeon/Character.java
+++ app/src/main/java/confusedgriffinproductions/dudesinadungeon/Character.java
@@ -143,6 +143,7 @@ public class Character {
         this.items = items;
     }
 
+    // Setters & Getters
     public int getId() {
         return id;
     }
--- app/src/main/java/confusedgriffinproductions/dudesinadungeon/Item.java
+++ app/src/main/java/confusedgriffinproductions/dudesinadungeon/Item.java
@@ -1,4 +1,33 @@
 package confusedgriffinproductions.dudesinadungeon;
 
 public class Item {
+
+    // Declare Item properties
+    private int id;
+    private String name;
+    private int price;
+    private String type;
+    private int dmg_def;
+
+    // Constructors
+    // Empty constructor
+    public Item() {}
+
+    // Constructor for weapons/shields
+    public Item(int id, String name, int price, String type, int dmg_def) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.type = type;
+        this.dmg_def = dmg_def;
+    }
+
+    // Constructor for non-weapons/shields
+    public Item(int id, String name, int price, String type) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.type = type;
+    }
+    
 }
