8e92a5fdfe5cd51294e792d65dd2ef5af3b0dc79
Nicholas Allaire <nicholas.allaire85@stclairconnect.ca>
2017-03-29 20:51:04 -0400

    #9: Created all Create methods for database-handler

--- app/src/main/java/confusedgriffinproductions/dudesinadungeon/DatabaseHandler.java
+++ app/src/main/java/confusedgriffinproductions/dudesinadungeon/DatabaseHandler.java
@@ -1,6 +1,8 @@
 package confusedgriffinproductions.dudesinadungeon;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -173,7 +175,160 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     }
 
     // CRUD OPERATIONS FOR THE DATABASE AND FOR THE TABLES
-    
+    /**
+     * Add new Objects to the database
+     */
+    /**
+     * Adds a new character to the database
+     * @param character - Character to be added to the database
+     */
+    public void addCharacter(Character character) {
+        // Get a writable Database
+        SQLiteDatabase db = this.getWritableDatabase();
+        // Create a ContentValues to store values
+        ContentValues values = new ContentValues();
+        // Put the values for the insert command
+        values.put(COLUMN_NAME, character.getName());
+        values.put(COLUMN_RACE, character.getRace());
+        values.put(COLUMN_CHAR_CLASS, character.getCharClass());
+        values.put(COLUMN_STRENGTH, character.getStrength());
+        values.put(COLUMN_AGILITY, character.getAgility());
+        values.put(COLUMN_RESILIENCE, character.getResilience());
+        values.put(COLUMN_LUCK, character.getLuck());
+        values.put(COLUMN_INTELLIGENCE, character.getIntelligence());
+        values.put(COLUMN_FIGHTING, character.getFighting());
+        values.put(COLUMN_GAMBLING, character.getGambling());
+        values.put(COLUMN_SHOOTING, character.getShooting());
+        values.put(COLUMN_LYING, character.getLying());
+        values.put(COLUMN_CASTING, character.getCasting());
+        values.put(COLUMN_ACROBATICS,character.getAcrobatics());
+        values.put(COLUMN_SNEAKING, character.getSneaking());
+        values.put(COLUMN_CRAFTING, character.getCrafting());
+        values.put(COLUMN_SURVIVAL, character.getSurvival());
+        // TODO: THIS MAY NEED TO BE FIXED
+        values.put(COLUMN_ITEMS, character.getItems().toString());
+        values.put(COLUMN_SPELLS, character.getSpells().toString());
+
+        // Execute the insert statement
+        db.insert(TABLE_CHARACTERS, null, values);
+        // Close the database
+        db.close();
+    }
+
+    /**
+     * Adds a new item to the database
+     * @param item - item to be added
+     */
+    public void addItem(Item item) {
+        // Get a writable Database
+        SQLiteDatabase db = this.getWritableDatabase();
+        // Create a ContentValues to store values
+        ContentValues values = new ContentValues();
+        // Put the values for the insert command
+        values.put(COLUMN_NAME, item.getName());
+        values.put(COLUMN_PRICE, item.getPrice());
+        values.put(COLUMN_TYPE, item.getType());
+        values.put(COLUMN_DESCRIPTION, item.getDescription());
+        values.put(COLUMN_DMG_DEF, item.getDmg_def());
+
+        // Execute the insert statement
+        db.insert(TABLE_ITEMS, null, values);
+        // Close the database
+        db.close();
+    }
+
+    /**
+     * Adds a new spell to the spell list
+     * @param spell - spell to be added
+     */
+    public void addSpell(Spell spell) {
+        // Get a writable Database
+        SQLiteDatabase db = this.getWritableDatabase();
+        // Create a ContentValues to store values
+        ContentValues values = new ContentValues();
+        // Put the values for the insert command
+        values.put(COLUMN_NAME, spell.getName());
+        values.put(COLUMN_DESCRIPTION, spell.getDescription());
+        values.put(COLUMN_SPELLTYPE, spell.getSpellType());
+        values.put(COLUMN_COMPONENTS, spell.getComponents());
+        values.put(COLUMN_EFFECTS, spell.getEffects());
+        values.put(COLUMN_DMG_HEAL, spell.getDmg_heal());
+
+        // Execute the insert statement
+        db.insert(TABLE_ITEMS, null, values);
+        // Close the database
+        db.close();
+    }
+
+    /**
+     * Adds a Portrait to the database
+     *
+     * @param portrait - portrait to be added
+     * @return integer (row location)
+     */
+    public int addPortrait(Portrait portrait) {
+        // Get a writable Database
+        SQLiteDatabase db = this.getWritableDatabase();
+        // Create a ContentValues to store values
+        ContentValues values = new ContentValues();
+        // Put the values for the insert command
+        values.put(COLUMN_RESOURCE, portrait.getResource());
+
+        // Execute the insert statement
+        db.insert(TABLE_PORTRAITS, null, values);
+        // Set the database to readable
+        db = this.getReadableDatabase();
+        // Return the row the portrait was added to
+        Cursor cursor = db.rawQuery("SELECT last_insert_rowid()", null);
+        if(cursor.moveToFirst()) {
+            int location = Integer.parseInt(cursor.getString(0));
+            System.out.println("Record ID " + location);
+            db.close();
+            return location;
+        }
+        return -1;
+    }
+
+    /**
+     * Method that will add a Character Portrait record
+     *
+     * @param portrait
+     * @param character
+     */
+    public void addCharacterPortrait(int portrait, int character){
+        // Get a writable Database
+        SQLiteDatabase db = this.getWritableDatabase();
+        // Create a ContentValues to store values
+        ContentValues values = new ContentValues();
+        // Put the values for the insert command
+        values.put(COLUMN_CHAR_ID, character);
+        values.put(COLUMN_PORTRAIT, portrait);
+
+        // Execute the insert statement
+        db.insert(TABLE_CHARACTER_PORTRAIT, null, values);
+        db.close();
+    }
+
+    /**
+     * Method that will add an Item Portrait record
+     *
+     * @param portrait
+     * @param item
+     */
+    public void addItemPortrait(int portrait, int item){
+        // Get a writable Database
+        SQLiteDatabase db = this.getWritableDatabase();
+        // Create a ContentValues to store values
+        ContentValues values = new ContentValues();
+        // Put the values for the insert command
+        values.put(COLUMN_ITEM_ID, item);
+        values.put(COLUMN_PORTRAIT, portrait);
+
+        // Execute the insert statement
+        db.insert(TABLE_ITEM_PORTRAIT, null, values);
+        db.close();
+    }
+
 
 
 
