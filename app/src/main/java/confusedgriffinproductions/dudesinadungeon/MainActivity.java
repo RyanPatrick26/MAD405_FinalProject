50f207e6bd2fcd08e02b901dfe28d353d98296c3
Nicholas Allaire <nicholas.allaire85@stclairconnect.ca>
2017-04-03 11:22:22 -0400

    #20: Added InteractionListeners for fragments

--- app/src/main/java/confusedgriffinproductions/dudesinadungeon/MainActivity.java
+++ app/src/main/java/confusedgriffinproductions/dudesinadungeon/MainActivity.java
@@ -1,9 +1,11 @@
 package confusedgriffinproductions.dudesinadungeon;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,7 +17,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,
+        MainFragment.OnFragmentInteractionListener,
+        CharacterListFragment.OnFragmentInteractionListener,
+        CreateCharacterFragment.OnFragmentInteractionListener,
+        ItemListFragment.OnFragmentInteractionListener,
+        SpellListFragment.OnFragmentInteractionListener{
 
     // Fragment manager to allow us to display, remove, and create fragments
     FragmentManager fm = getSupportFragmentManager();
@@ -84,8 +91,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
+        // If Else Statement to navigate to each fragment throught the nav drawer
         if (id == R.id.nav_about) {
-            
+
         } else if (id == R.id.nav_create_char) {
 
         } else if (id == R.id.nav_view_char) {
@@ -105,5 +113,15 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    /**
+     * Empty method to allow interaction between fragments
+     *
+     * @param uri
+     */
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
