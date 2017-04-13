bfd48aaf4d638cade978b71e157df6b5f905ce80
Nicholas Allaire <nicholas.allaire85@stclairconnect.ca>
2017-04-13 11:28:05 -0400

    #4: Created PreferencesFragment within SettingsActivity

--- app/src/main/java/confusedgriffinproductions/dudesinadungeon/SettingsActivity.java
+++ app/src/main/java/confusedgriffinproductions/dudesinadungeon/SettingsActivity.java
@@ -1,12 +1,27 @@
 package confusedgriffinproductions.dudesinadungeon;
 
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new DudesInADungeonPreferencesFragment()).commit();
     }
+
+    // Create a class for the
+    public static class DudesInADungeonPreferencesFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+
+    }
+
 }
